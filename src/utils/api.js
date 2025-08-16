ef7e17f563e55c3d9566cb3b817bc7b13dcf761b
Seunga Jeong <jsa3341@gmail.com>
2025-08-16 19:33:05 +0900

    make review page

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-// 상황따라 주소 다름
-// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
-// const REACT_APP_PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
-// console.log("proxy", BACKEND_PROXY);
+
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
   headers: {
